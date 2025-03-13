3e7de427f315d97a6295f7af1e369b97b8f73027
Gagandeep010 <program25gdc@gmail.com>
2025-03-13 15:22:39 +0530

    small-changes

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './Reasons.css'
 import image1 from "../../assets/image1.png"
-import image2 from "../../assets/image2.png"
-import image3 from "../../assets/image3.png"
-import image4 from "../../assets/image4.png"
+import image2 from "../../assets/yoga2.jpg"
+import image3 from "../../assets/yoga1.jpg"
+import image4 from "../../assets/image2.png"
 import nb from "../../assets/nb.png"
 import nike from "../../assets/nike.png"
 import adidas from "../../assets/adidas.png"
